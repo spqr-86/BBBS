b51add2fd1bfc22633e18a2bbdaa03b47afc9725
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-15 20:03:40 +0700

    added place, tag models to the admin

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place, Tag
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'name', 'info', 'description', 'imageUrl', 'link')
+    search_fields = ('title', 'name', 'info')
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
