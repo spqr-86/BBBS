56757becea142660b0ab29c7be8ab3f39c9dc66a
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-15 18:26:14 +0700

    added default value for EMAIL_PORT

--- project/project/settings.py
+++ project/project/settings.py
@@ -147,6 +147,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', default=587))
 EMAIL_USE_SSL = int(os.getenv('EMAIL_USE_SSL', default=False))
 EMAIL_USE_TLS = int(os.getenv('EMAIL_USE_TLS', default=False))
