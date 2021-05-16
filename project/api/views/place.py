ce365733b23ad4d3bb1d46a3a89b0dcafb070361
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-16 22:35:23 +0700

    added custom ViewSet and search_field

--- project/api/views/place.py
+++ project/api/views/place.py
@@ -1,12 +1,20 @@
-from rest_framework import viewsets, permissions
+from rest_framework import mixins, viewsets, permissions
 
 from ..models import Place
 from ..serializers import PlaceSerializer
 
 
-class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
+class ListCreateDelViewSet(mixins.CreateModelMixin,
+                           mixins.DestroyModelMixin,
+                           mixins.ListModelMixin,
+                           viewsets.GenericViewSet):
+    pass
+
+
+class PlaceViewSet(ListCreateDelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
+    search_fields = ['name']
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly,
     ]
