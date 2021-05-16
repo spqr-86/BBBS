1a3a2559410e159e2cb7354ddb7b79f59eef27ad
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-16 12:38:22 +0700

    fixed the DEBUG variable

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = int(os.environ.get('DEBUG', default=True))
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
