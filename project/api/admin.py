ccf13f85570cbbd948408c314abce9a6858a4e07
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-18 20:57:13 +0700

    gettext replaced by gettext_lazy

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib import admin
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from .fields import fields
 from .models import Article, History, Movie, Place, Question, Tag, Video
