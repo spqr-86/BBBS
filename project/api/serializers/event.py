e06e2a6608684169d80e352c6a15302c2b10ee00
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-21 22:03:57 +0700

    get_booked method was removed

--- project/api/serializers/event.py
+++ project/api/serializers/event.py
@@ -7,11 +7,8 @@ from .tag import TagSerializer
 class EventSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False, read_only=True)
     taken_seats = serializers.IntegerField(read_only=True)
-    booked = serializers.SerializerMethodField()
+    booked = serializers.BooleanField(read_only=True)
 
     class Meta:
         exclude = ['participant']
         model = Event
-
-    def get_booked(self, obj):
-        return Event.objects.filter(participant=self.context['user']).exists()
