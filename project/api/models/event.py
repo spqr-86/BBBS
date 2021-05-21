1e5053d6c3823e69853e3049f2fdd492b5a8a730
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-21 23:11:02 +0700

    added validation of the start_at and end_at fields

--- project/api/models/event.py
+++ project/api/models/event.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -21,6 +22,7 @@ class Event(models.Model):
     )
     description = models.TextField(
         verbose_name=_('Описание мероприятия'),
+        max_length=1000,
     )
     start_at = models.DateTimeField(
         verbose_name=_('Время начала'),
@@ -28,7 +30,7 @@ class Event(models.Model):
     end_at = models.DateTimeField(
         verbose_name=_('Время окончания'),
     )
-    seats = models.IntegerField(
+    seats = models.PositiveSmallIntegerField(
         verbose_name=_('Максимальное число участников'),
     )
     city = models.ForeignKey(
@@ -49,6 +51,14 @@ class Event(models.Model):
         ordering = ['id']
         verbose_name = _('Событие')
         verbose_name_plural = _('События')
+
+    def clean(self):
+        errors = {}
+        if self.start_at > self.end_at:
+            errors['end_at'] = ValidationError(
+                _('Конец события не может быть раньше начала'))
+        if errors:
+            raise ValidationError(errors)
 
     def __str__(self):
         return self.title
