e647904004001d0d321c0ef2216b77c31f10629c
Evgeniy Khlystov <hlystovea@gmail.com>
2021-05-21 22:05:07 +0700

    another queryset with 'booked' field

--- project/api/views/event.py
+++ project/api/views/event.py
@@ -1,27 +1,32 @@
-from django.db.models import Count
+from django.db.models import Count, Exists, OuterRef
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 
 from ..models import Event
+from ..permissions import IsUsersCity
 from ..serializers import EventSerializer
 
 
 class EventViewSet(viewsets.ViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsUsersCity, permissions.IsAuthenticated]
 
     def list(self, request):
         user = request.user
+        booked = Event.objects.filter(pk=OuterRef('pk'), participant=user)
         queryset = Event.objects.filter(
             city=user.profile.city).annotate(
-                taken_seats=Count('participant'))
-        serializer = EventSerializer(
-            queryset, many=True, context={'user': user})
+                taken_seats=Count('participant')).annotate(
+                    booked=Exists(booked))
+        serializer = EventSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        user = request.user
-        queryset = Event.objects.annotate(taken_seats=Count('participant'))
-        event = get_object_or_404(queryset, pk=pk, city=user.profile.city)
-        serializer = EventSerializer(event, context={'user': request.user})
+        booked = Event.objects.filter(pk=pk, participant=request.user)
+        queryset = Event.objects.annotate(
+                taken_seats=Count('participant')).annotate(
+                    booked=Exists(booked))
+        event = get_object_or_404(queryset, pk=pk)
+        self.check_object_permissions(request, event)
+        serializer = EventSerializer(event)
         return Response(serializer.data)
