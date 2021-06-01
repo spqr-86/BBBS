d66a32388f9a0793599a84515db0a7ee1f5d328f
Артем Скачков <topkzzz@gmail.com>
2021-06-01 22:13:15 +0300

    Update get_events in main Views

--- project/api/views/main.py
+++ project/api/views/main.py
@@ -1,22 +1,25 @@
 from django.db.models import Count, Exists, F, OuterRef
 from django.utils.timezone import now
+from rest_framework.generics import RetrieveAPIView, get_object_or_404
 from rest_framework.permissions import AllowAny
-from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 
-from ..models import Event, Main
+from ..models import City, Event, Main
 from ..serializers.main import MainSerializer
 
 
 def get_events(request):
     user = request.user
-    queryset = Event.objects.filter(end_at__gt=now()) \
+    if user.is_authenticated:
+        city = user.city
+    else:
+        city = get_object_or_404(City, id=request.GET.get('city'))
+    queryset = Event.objects.filter(end_at__gt=now(), city=city) \
                     .annotate(remain_seats=F('seats')-Count('participants'))
     if user.is_authenticated:
         booked = Event.objects.filter(pk=OuterRef('pk'), participants=user)
-        queryset = queryset.filter(city=user.city) \
-                           .annotate(booked=Exists(booked))
-    return queryset.order_by('start_at')
+        queryset = queryset.annotate(booked=Exists(booked))
+    return queryset
 
 
 class MainViewSet(RetrieveAPIView):
