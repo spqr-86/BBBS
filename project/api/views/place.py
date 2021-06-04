3e4d44a1cd3e3e84db37499ffbce56a57d5d8358
Артем Скачков <topkzzz@gmail.com>
2021-06-04 20:42:19 +0300

    BBBS-30 Complete

--- project/api/views/place.py
+++ project/api/views/place.py
@@ -1,12 +1,12 @@
 from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from ..models import Place
 from ..serializers import PlaceSerializer
 
 
 class PlaceViewSet(viewsets.ReadOnlyModelViewSet):
-    # TODO add pagination
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     permission_classes = [permissions.AllowAny]
-    pagination_class = None
+    pagination_class = PageNumberPagination
