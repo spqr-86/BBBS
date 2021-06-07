2e7efdc64101c2d8c1533ccceb85f32b8192da62
Артем Скачков <topkzzz@gmail.com>
2021-06-08 00:13:50 +0300

    BBBS-36 completed. + POST method.

--- project/api/views/question.py
+++ project/api/views/question.py
@@ -1,12 +1,19 @@
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
 
 from ..models import Question
 from ..serializers import QuestionSerializer
 
 
-class QuestionViewSet(ReadOnlyModelViewSet):
+class QuestionViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin
+):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
