b0457eb159457f6a2b67193565db14cb54641dc6
Артем Скачков <topkzzz@gmail.com>
2021-06-08 00:09:07 +0300

    BBBS-34 completed.

--- project/api/views/question.py
+++ project/api/views/question.py
@@ -1,5 +1,6 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
 from ..models import Question
 from ..serializers import QuestionSerializer
@@ -8,5 +9,5 @@ from ..serializers import QuestionSerializer
 class QuestionViewSet(ReadOnlyModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    permission_classes = [AllowAny]
-    pagination_class = None
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = PageNumberPagination
