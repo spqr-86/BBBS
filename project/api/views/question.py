bdc2fd451eb196d587abc490bdd23ea1751f4d1a
Артем Скачков <topkzzz@gmail.com>
2021-06-08 00:24:25 +0300

    BBBS-35 and BBBS-49 complete.

--- project/api/views/question.py
+++ project/api/views/question.py
@@ -1,10 +1,14 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from ..models import Question
+from ..models import Question, Tag
 from ..serializers import QuestionSerializer
+from .tag import TagSerializer
 
 
 class QuestionViewSet(
@@ -18,3 +22,11 @@ class QuestionViewSet(
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = PageNumberPagination
+    filterset_backends = [DjangoFilterBackend]
+    filterset_fields = ['tags']
+
+    @action(methods=['get'], detail=False)
+    def tags(self, request):
+        tags = Tag.objects.filter(questions__isnull=False)
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
