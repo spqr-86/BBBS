329c53ed24e53820ede3ab3cb3aa6174e17bdab1
Артем Скачков <topkzzz@gmail.com>
2021-06-08 20:32:34 +0300

    Add mixins for viewsets

--- project/api/views/right.py
+++ project/api/views/right.py
@@ -1,25 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from ..models import Right, Tag
-from ..serializers import RightSerializer, TagSerializer
+from ..models import Right
+from ..serializers import RightSerializer
+
+from . import TagMixin
 
 
-class RightViewSet(ReadOnlyModelViewSet):
+class RightViewSet(ReadOnlyModelViewSet, TagMixin):
     queryset = Right.objects.all()
     serializer_class = RightSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['tags', ]
     permission_classes = [AllowAny]
     pagination_class = PageNumberPagination
-
-    @action(methods=['get'], detail=False)
-    def tags(self, request):
-        tags_id = Right.objects.values_list('tags', flat=True)
-        tags = Tag.objects.filter(pk__in=tags_id)
-        serializer = TagSerializer(tags, many=True)
-        return Response(serializer.data)
