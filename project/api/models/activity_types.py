78f7e51d483d17207faed64e8aa19654c3bcfcda
Evgeniy Khlystov <hlystovea@gmail.com>
2021-06-09 20:52:26 +0700

    Changed ActivityType model

--- project/api/models/activity_types.py
+++ project/api/models/activity_types.py
@@ -5,5 +5,15 @@ from django.utils.translation import gettext_lazy as _
 class ActivityType(models.Model):
     name = models.CharField(
         verbose_name=_('Вид активности'),
-        max_length=200
+        max_length=30,
+        unique=True,
     )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('id',)
+        verbose_name = _('Тип отдыха')
+        verbose_name_plural = _('Типы отдыха')
+
+    def __str__(self):
+        return self.name
