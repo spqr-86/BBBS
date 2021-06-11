966e0415d0aa2a37c8ce811baca8973af80a2f76
Evgeniy Khlystov <hlystovea@gmail.com>
2021-06-11 19:31:40 +0700

    changed history model

--- project/api/models/history.py
+++ project/api/models/history.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
 
 
 class History(models.Model):
@@ -7,10 +11,25 @@ class History(models.Model):
         verbose_name=_('Заголовок'),
         max_length=200,
     )
+    mentor = models.ForeignKey(
+        User,
+        verbose_name=_('Наставник'),
+        on_delete=models.CASCADE,
+    )
+    child = models.CharField(
+        verbose_name=_('Имя ребёнка'),
+        max_length=100,
+    )
+    together_since = models.DateField(
+        verbose_name=_(''),
+    )
     image_url = models.URLField(
         verbose_name=_('Изображение'),
         blank=True,
         null=True,
+    )
+    description = models.TextField(
+        verbose_name=_('Текст истории'),
     )
     output_to_main = models.BooleanField(
         verbose_name=_('Отображать на главной странице'),
@@ -22,6 +41,11 @@ class History(models.Model):
         ordering = ('id',)
         verbose_name = _('История')
         verbose_name_plural = _('Истории')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['mentor', 'child'],
+                name='mentor_and_child_uniq_together'),
+            ]
 
     def __str__(self):
         return self.title
--- project/api/views/history.py
+++ project/api/views/history.py
@@ -1,12 +1,16 @@
-from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import permissions
+from rest_framework.pagination import PageNumberPagination
 
+from . import GetListPostPutMixin
 from ..models import History
 from ..serializers import HistorySerializer
 
 
-class HistoryViewSet(ReadOnlyModelViewSet):
+class HistoryViewSet(GetListPostPutMixin):
     queryset = History.objects.all()
     serializer_class = HistorySerializer
-    permission_classes = [AllowAny]
-    pagination_class = None
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(mentor=self.request.user)
