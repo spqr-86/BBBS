c5f5b357e1b3a423cc7631677e8b5bcf274e59e5
Evgeniy Khlystov <hlystovea@gmail.com>
2021-06-11 22:56:26 +0700

    extended validator

--- project/api/models/validators.py
+++ project/api/models/validators.py
@@ -5,7 +5,11 @@ from django.utils.translation import gettext_lazy as _
 def age_validator(value):
     if value > 25:
         raise ValidationError(
-            _('Упс.. ребенок уже слишком взрослый'),
+            _('Слишком большой возраст для ребёнка'),
             code='invalid',
-            params={'value': value},
-        )
+            params={'value': value})
+    elif value < 0:
+        raise ValidationError(
+            _('Возраст не может быть меньше 0'),
+            code='invalid',
+            params={'value': value})
