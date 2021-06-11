6c412b6e835b77dfa3c94c7e0fe3d1160a3c195a
Evgeniy Khlystov <hlystovea@gmail.com>
2021-06-11 22:51:44 +0700

    added mentor field

--- project/api/serializers/history.py
+++ project/api/serializers/history.py
@@ -1,8 +1,21 @@
+from rest_framework import validators
+from rest_framework.serializers import CurrentUserDefault
+
 from .base import BaseSerializer
+from .profile import MentorSerializer
 from ..models import History
 
 
 class HistorySerializer(BaseSerializer):
+    mentor = MentorSerializer(default=CurrentUserDefault())
+
     class Meta(BaseSerializer.Meta):
         tags = None
         model = History
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=History.objects.all(),
+                fields=['mentor', 'child'],
+                message='Такая пара наставник - ребенок уже добавлена'
+            )
+        ]
--- project/api/views/history.py
+++ project/api/views/history.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 from rest_framework.pagination import PageNumberPagination
 
-from . import GetListPostPutMixin
+from .mixins import GetListPostPutMixin
 from ..models import History
 from ..serializers import HistorySerializer
 
@@ -11,6 +11,3 @@ class HistoryViewSet(GetListPostPutMixin):
     serializer_class = HistorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     pagination_class = PageNumberPagination
-
-    def perform_create(self, serializer):
-        serializer.save(mentor=self.request.user)
