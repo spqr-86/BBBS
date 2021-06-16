431fbafb032acba2b5e7dac4ef06d18efe14d563
Evgeniy Khlystov <hlystovea@gmail.com>
2021-06-16 23:04:15 +0700

    added special filters for Place

--- project/api/views/place.py
+++ project/api/views/place.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 from rest_framework.pagination import PageNumberPagination
 
+from ..filters import PlaceFilter
 from ..models import Place
 from ..serializers import PlaceSerializer
 from .mixins import GetListPostPutMixin, TagMixin
@@ -11,6 +12,7 @@ class PlacesViewSet(GetListPostPutMixin, TagMixin):
     serializer_class = PlaceSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     pagination_class = PageNumberPagination
+    filter_class = PlaceFilter
 
     def perform_create(self, serializer):
         serializer.save(chosen=self.request.user.is_mentor)
