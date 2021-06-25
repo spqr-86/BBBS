cf57d4efa3da405d171f3fa9785cee1d2b62e5db
Andrew <Andrey18651@gmail.com>
2021-06-25 19:43:10 +0300

    delete verification order field in api/utils/castraitor.py

--- project/api/utils/castraitor.py
+++ project/api/utils/castraitor.py
@@ -10,19 +10,11 @@ class Castraitor:
         self.n = n
         self.order_field = order_field
         self.check_n()
-        self.check_order_field()
 
     def check_n(self):
         if self.n <= 0:
             raise InvalidCountException(self.model)
         return self.n
-
-    def check_order_field(self):
-        fields_from_model = dir(self.model)
-        true_order_field = self.order_field.replace('-', '')
-        if true_order_field not in fields_from_model:
-            raise Exception()
-        return self.order_field
 
     def get_n_records(self) -> QuerySet:
         all_model_records = self.model.objects.all()
