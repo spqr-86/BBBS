e3dd6e09692b237874f41e47877a645b5c2ce285
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-01 23:42:47 +0700

    return to the TagMixin

--- project/api/views/question.py
+++ project/api/views/question.py
@@ -1,34 +1,14 @@
-from django_filters.filters import CharFilter
-from django_filters.filterset import FilterSet
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-
-from ..models import Question, Tag
-from ..serializers import QuestionSerializer, TagSerializer
-from . import GetListPostPutMixin
 
 
-class TagsFilter(FilterSet):
-    tags = CharFilter(field_name='tags__slug', method='filter_tags')
-
-    def filter_tags(self, queryset, slug, tags):
-        return queryset.filter(tags__slug__in=(tags.split(',')))
+from ..models import Question
+from ..serializers import QuestionSerializer
+from . import GetListPostPutMixin, TagMixin
 
 
-class QuestionViewSet(GetListPostPutMixin):
+class QuestionViewSet(GetListPostPutMixin, TagMixin):
     queryset = Question.objects.exclude(answer=None)
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = LimitOffsetPagination
-    filterset_backends = [DjangoFilterBackend]
-    filter_class = TagsFilter
-
-    @action(methods=['get'], detail=False)
-    def tags(self, request):
-        queryset = self.get_queryset()
-        tags = Tag.objects.filter(questions__in=queryset).distinct()
-        serializer = TagSerializer(tags, many=True)
-        return Response(serializer.data)
