acf6c2c4dcf38e9738fa6004b02e4219e328f197
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-04 18:42:13 +0700

    added tags field for the Event model, changed serializers

--- project/api/serializers/event.py
+++ project/api/serializers/event.py
@@ -6,18 +6,9 @@ from .tag import TagSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
-    remain_seats = serializers.IntegerField(read_only=True)
-    booked = serializers.BooleanField(default=False, read_only=True)
-
-    class Meta:
-        model = Event
-        exclude = ['participants']
-
-
-class MainEventSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     remain_seats = serializers.IntegerField(read_only=True)
-    booked = serializers.BooleanField(default=False, read_only=True)
+    booked = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Event
@@ -31,7 +22,13 @@ class DateEventSerializer(serializers.Serializer):
         fields = '__all__'
 
 
-class ParticipantSerializer(serializers.ModelSerializer):
+class ProfileEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'start_at']
+
+
+class ParticipantWriteSerializer(serializers.ModelSerializer):
     participant = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
@@ -46,3 +43,11 @@ class ParticipantSerializer(serializers.ModelSerializer):
                 message=_('Вы уже зарегестрированы на это событие')
             )
         ]
+
+
+class ParticipantReadSerializer(serializers.ModelSerializer):
+    event = ProfileEventSerializer()
+
+    class Meta:
+        model = Participant
+        fields = ['id', 'event']
