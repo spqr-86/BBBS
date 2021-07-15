fcfc87976f6d20e918efa6b57b8ff4613a723c1f
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-15 21:43:52 +0700

    added filtering by resource_group for videos

--- project/api/filters.py
+++ project/api/filters.py
@@ -1,7 +1,7 @@
 from django_filters import CharFilter, FilterSet, NumberFilter
 from django_filters.filters import BooleanFilter
 
-from .models import Event, Place
+from .models import Event, Place, Video
 
 
 class PlaceFilter(FilterSet):
@@ -42,3 +42,17 @@ class EventFilter(FilterSet):
     class Meta:
         model = Event
         fields = ['months', 'years']
+
+
+class VideoFilter(FilterSet):
+    tags = CharFilter(field_name='tags__slug', method='filter_tags')
+    resource_group = BooleanFilter(field_name='resource_group')
+
+    def filter_tags(self, queryset, slug, tags):
+        return queryset.filter(
+            tags__slug__in=tags.split(',')
+        ).distinct()
+
+    class Meta:
+        model = Video
+        fields = ['tags', 'resource_group']
