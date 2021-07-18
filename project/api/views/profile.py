388de72479a51f562df60e12fef63310d0b41d35
Andrew <Andrey18651@gmail.com>
2021-07-18 20:37:22 +0300

    add SendMailViewSet in profile.py: send password hash on mail by sending email query param

--- project/api/views/profile.py
+++ project/api/views/profile.py
@@ -1,8 +1,15 @@
 from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import RetrieveUpdateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from ..serializers import ProfileSerializer
+from ..utils.BaseViewSet import BaseAttendersView
+from ..utils.email import send_email
+from ...account.models import CustomUser
 
 User = get_user_model()
 
@@ -14,3 +21,16 @@ class ProfileViewSet(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class SendMailViewSet(BaseAttendersView):
+    permission_classes_by_action = {
+        'send_password': [AllowAny]
+    }
+
+    @method_decorator(csrf_exempt)
+    def send_password(self, request: Request):
+        email = self.request.query_params['email']
+        user = CustomUser.objects.get(email=email)
+        send_email([user.email], 'password', user.password)
+        return Response({'sent': 'true'})
