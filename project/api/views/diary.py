7238f3b965657d582ad160f2fb6ce4a6253bf5c0
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-19 21:24:26 +0700

    added EmailSerializer

--- project/api/views/diary.py
+++ project/api/views/diary.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 
 from ..models import Diary
 from ..permissions import IsOwner
-from ..serializers import DiarySerializer
+from ..serializers import DiarySerializer, EmailSerializer
 from ..utils.email import send_email
 
 
@@ -30,23 +30,25 @@ class DiaryViewSet(viewsets.ModelViewSet):
         diary = get_object_or_404(Diary, pk=pk, mentor=mentor)
         if not diary.sent_to_curator:
             email = mentor.curator.email
-            subject = f'{mentor.first_name} {mentor.last_name[:1]}.: \
-                        запись в дневнике от {diary.date.isoformat()}'
-            text = diary.description
-            try:
-                send_email(email, subject, text)
-            except RequestException:
-                message = {
-                    'diary': _('Проблемы с отправкой, попробуйте позднее.')
-                }
-                return Response(
-                    message,
-                    status=status.HTTP_504_GATEWAY_TIMEOUT
-                )
-            diary.sent_to_curator = True
-            diary.save()
-            message = {'diary': _('Запись дневника отправлена куратору.')}
-            return Response(message, status=status.HTTP_200_OK)
+            serializer = EmailSerializer(data={'email': email})
+            if serializer.is_valid(raise_exception=True):
+                subject = f'{mentor.first_name} {mentor.last_name[:1]}.: \
+                            запись в дневнике от {diary.date.isoformat()}'
+                text = diary.description
+                try:
+                    send_email(email, subject, text)
+                except RequestException:
+                    message = {
+                        'diary': _('Проблемы с отправкой, попробуйте позднее.')
+                    }
+                    return Response(
+                        message,
+                        status=status.HTTP_504_GATEWAY_TIMEOUT
+                    )
+                diary.sent_to_curator = True
+                diary.save()
+                message = {'diary': _('Запись дневника отправлена куратору.')}
+                return Response(message, status=status.HTTP_200_OK)
         message = {
             'diary': _('Запись дневника уже была отправлена куратору ранее.')
         }
